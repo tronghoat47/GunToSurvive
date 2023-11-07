c9e1659893f458708db3549c649cbfec0807e979
HoaTT <hoa.truongtrong26@gmail.com>
2023-11-07 21:46:56 +0700

    Add skill - Edit camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -46,6 +46,16 @@ public class PlayerController : MonoBehaviour
 
     public PauseMenu pauseMenu;
 
+
+    //Attribute Skill
+    private GameObject skill;
+    private bool isRotating = false;
+    private bool isSkillVisible = false;
+    private float currentRotation = 0f;
+    private float rotationSpeed;
+
+
+
     void Start()
     {
         animator = GetComponentInChildren<Animator>();
@@ -60,6 +70,13 @@ public class PlayerController : MonoBehaviour
         healthBar.fillAmount = currentHP / maxHP;
         sheildBar.fillAmount = currentSheild / maxSheild;
         manaBar.fillAmount = currentMana / maxMana;
+
+        skill = transform.Find("Skill").gameObject;
+        if (skill != null)
+        {
+            skill.SetActive(false);
+        }
+        rotationSpeed = 360f;
     }
 
     void Update()
@@ -69,10 +86,13 @@ public class PlayerController : MonoBehaviour
 
         transform.position += moveInput * moveSpeed * Time.deltaTime;
 
-        
+
         animator.SetFloat("Speed", moveInput.sqrMagnitude);
 
-        if(Input.GetKeyDown(KeyCode.Space) && rollTime <= 0){
+
+        //Use roll
+        if (Input.GetKeyDown(KeyCode.C) && rollTime <= 0)
+        {
             currentMana -= Constants.manaRollOver;
             manaText.text = currentMana.ToString();
             moveSpeed += rollBoost;
@@ -81,30 +101,69 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("Roll", true);
         }
 
+        //Use skill
+        if (Input.GetKeyDown(KeyCode.Space) && !isRotating)
+        {
+            isRotating = true;
+            ShowSkill();
+        }
+        if (isRotating)
+        {
+            if (skill != null)
+            {
+                // Calculate the rotation around the player
+                Vector3 playerPosition = transform.position;
+                Vector3 swordPosition = skill.transform.position;
+                Vector3 directionToSword = swordPosition - playerPosition;
+
+                // Update the current rotation
+                currentRotation += rotationSpeed * Time.deltaTime;
+                if (currentRotation >= 360f)
+                {
+                    // Reset rotation
+                    currentRotation = 0f;
+                    isRotating = false;
+                    isSkillVisible = false;
+                    skill.SetActive(false);
+                }
+
+                // Apply the rotation to the sword
+                skill.transform.rotation = Quaternion.Euler(0, 0, currentRotation);
+
+            }
+        }
+
         // pause screen
-        if (Input.GetKeyDown(KeyCode.Escape)){
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
             pauseMenu.Pause();
         }
 
-        if(rollTime <= 0 && rollOnce == true){
+        if (rollTime <= 0 && rollOnce == true)
+        {
             animator.SetBool("Roll", false);
             moveSpeed -= rollBoost;
             rollOnce = false;
-        }else{
+        }
+        else
+        {
             rollTime -= Time.deltaTime;
         }
 
-        if (moveInput.x != 0){
+        if (moveInput.x != 0)
+        {
             if (moveInput.x > 0)
                 characterSR.transform.localScale = new Vector3(1f, 1f, 0);
             else
                 characterSR.transform.localScale = new Vector3(-1f, 1f, 0);
         }
 
-        if(currentHP > Constants.maxBloodPlayer) {
+        if (currentHP > Constants.maxBloodPlayer)
+        {
             currentHP = Constants.maxBloodPlayer;
         }
-        if (currentSheild < 0) {
+        if (currentSheild < 0)
+        {
             currentSheild = 0;
         }
 
@@ -115,14 +174,24 @@ public class PlayerController : MonoBehaviour
         sheildText.text = "Sheild: " + currentSheild.ToString();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision) {
+    private void ShowSkill()
+    {
+        isSkillVisible = true;
+        skill.SetActive(true);
+    }
 
-        if(collision.gameObject.tag == "moresheild") {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "moresheild")
+        {
             currentSheild += Constants.moreSheild;
         }
 
-        if (currentSheild > 0) {
-            switch(collision.tag) {
+        if (currentSheild > 0)
+        {
+            switch (collision.tag)
+            {
                 case "bulletEnemy":
                     currentSheild -= Constants.bulletEnemyDame;
                     break;
@@ -130,12 +199,15 @@ public class PlayerController : MonoBehaviour
                     currentSheild -= Constants.enemyDame;
                     break;
                 default
-                    : break;
+                    :
+                    break;
             }
         }
 
-        if (currentHP > 0 && currentSheild <=0 ) {
-            switch(collision.tag) {
+        if (currentHP > 0 && currentSheild <= 0)
+        {
+            switch (collision.tag)
+            {
                 case "chicken":
                     currentHP += Constants.moreHealthChicken;
                     break;
@@ -153,7 +225,8 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        collision.gameObject.SetActive(false);
+        if (collision.gameObject.tag != "Bullet" && collision.gameObject.tag != "Skill")
+            collision.gameObject.SetActive(false);
 
     }
 
